5b7f92755504066f80625a05d22b60702cd83958
Raymart Paraiso <allanraymart.paraiso.dev@gmail.com>
2026-08-19 20:59:35 +0800

    updates on login blade

--- resources/views/native/auth/login.blade.php
+++ resources/views/native/auth/login.blade.php
@@ -7,21 +7,22 @@
                     alt="Glenz Ruby Store Logo" class="w-40 h-40 object-contain self-center" />
                 <native:text :font-weight="8" class="text-2xl mb-4 text-blue-900">GLENZRUBY STORE</native:text>
 
-                @if(!empty($generalError))
-                    <native:column class="w-full p-3 mb-3 bg-red-100 dark:bg-red-900/30 rounded-xl border border-red-400">
+                @if (!empty($generalError))
+                    <native:column
+                        class="w-full p-3 mb-3 bg-red-100 dark:bg-red-900/30 rounded-xl border border-red-400">
                         <native:text class="text-red-600 dark:text-red-400 text-sm font-medium">
                             {{ $generalError }}
                         </native:text>
                     </native:column>
                 @endif
 
-                <native:outlined-text-input size="sm" label="Username" native:model.blur="username"
-                    leading-icon="person" class="w-full mb-3 text-theme-on-surface"
-                    :is-error="!empty($usernameError)" :supporting="$usernameError" />
+                <native:outlined-text-input size="sm" label="Username" native:model.debounce.300ms="username"
+                    leading-icon="person" class="w-full mb-3 text-theme-on-surface" :is-error="!empty($usernameError)"
+                    :supporting="$usernameError" />
 
-                <native:outlined-text-input size="sm" label="Password" native:model.blur="password"
-                    leading-icon="lock" class="w-full mb-4" secure
-                    :is-error="!empty($passwordError)" :supporting="$passwordError" />
+                <native:outlined-text-input size="sm" label="Password" native:model.debounce.300ms="password"
+                    leading-icon="lock" class="w-full mb-4" secure :is-error="!empty($passwordError)"
+                    :supporting="$passwordError" />
 
                 <native:button label="Login" icon="login" size="lg" @press="verify" class="w-full"
                     loading="{{ $isLoading }}" />
